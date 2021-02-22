e073d2df83b2fa2f01d6a56e6abce2c2586ac6a3
Ray C <rayc2045@gmail.com>
2021-02-23 01:57:41 +0800

    Replace "request" with "axios"

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const puppeteer = require('puppeteer-core');
 const fs = require('fs');
-const request = require('request');
+const axios = require('axios');
 
 (async () => {
   const browser = await puppeteer.launch({
     executablePath: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
     args: [
-      // '--incognito', // 無痕模式
+      '--incognito', // 無痕模式
       // '--no-sandbox',
       // 'disable-setuid-sandbox',
       '--window-size=800,800'
     ],
-    headless: false, // Open browser
+    // headless: false, // Open browser
     slowMo: 100
   });
 
   const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36');
   // page.setViewport({ width: 800, height: 1200, deviceScaleFactor: 1})
   await page.goto('https://rayc2045.github.io/ghibli-gallery/', {
     waitUntil: 'domcontentloaded'
@@ -25,10 +26,10 @@ const request = require('request');
   const buttons = await page.evaluate(() =>
     [...document.querySelectorAll('.catalog a')]);
 
-  // for (const i in buttons) {
+  for (const i in buttons) {
     // 點擊按鈕，等待
     await page.evaluate(() =>
-      [...document.querySelectorAll('.catalog a')][0].click());
+      [...document.querySelectorAll('.catalog a')][i].click());
 
     // Get the height of the rendered page
     const bodyHandle = await page.$('body');
@@ -53,11 +54,11 @@ const request = require('request');
 
     // Make folder and download images
     const folderName = await page.evaluate(() =>
-      document.querySelector('#title').textContent);
+      document.querySelector('#title').textContent.trim());
 
     const imageLinks = await page.evaluate(() =>
       [...document.querySelectorAll('img')]
-        .map(img => img.src)
+        .map(img => img.src) // https://www.ghibli.jp/gallery/redturtle001.jpg
         .filter(link => !link.includes('rayc2045'))
     );
 
@@ -76,25 +77,32 @@ const request = require('request');
         console.log(`Download "${folderName}" ${imageName}`);
       });
     });
-  // }
+  }
 
-  // await browser.close();
+  await browser.close();
 })();
 
 function wait(sec) {
-  return new Promise(resolve => setTimeout(() => resolve()), sec * 1000)
+  return new Promise(resolve => setTimeout(() => resolve()), sec * 1000);
 }
 
 function createFolder(dirPath) {
   if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath);
 }
 
-function downloadImage(url, path, callback) {
-  request.head(url, (err, res, body) => {
-    // console.log(`content-type: ${res.headers['content-type']}`);
-    // console.log(`content-length: ${res.headers['content-length']}`);
-    request(url)
-      .pipe(fs.createWriteStream(path))
-      .on('close', callback);
+async function downloadImage(url, path, callback) {
+  const writer = fs.createWriteStream(path);
+
+  const response = await axios({
+    url,
+    methods: 'GET',
+    responseType: 'stream'
   });
+  
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  })
 }
